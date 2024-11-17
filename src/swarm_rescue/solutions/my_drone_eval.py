feb2a01032944cb66cad73dbef3d5c39a34dcff3
Younes-speed <younes.oudina@gmail.com>
2024-11-17 08:22:32 +0100

    INITIALISATION : - création de my_drone_first.py dans ./src/solutions - création des fonctions à implémenter ce sont des exemples de ce que ça doit être bien sur vous pouvez modifier si vous le voulez

--- src/swarm_rescue/solutions/my_drone_eval.py
+++ src/swarm_rescue/solutions/my_drone_eval.py
@@ -1,5 +1,8 @@
 from solutions.my_drone_random import MyDroneRandom
+from solutions.my_drone_motionless import MyDroneMotionless
+from solutions.my_drone_lidar_communication import MyDroneLidarCommunication
 
 
-class MyDroneEval(MyDroneRandom):
+class MyDroneEval(MyDroneLidarCommunication):
     pass
+
